11ffb7f4fe04dbbcfb79a47f8069fd157b9637c0
Nils-Börge Margotti <nilsmargotti@gmail.com>
2018-10-18 17:10:43 +0200

    Add travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl --url 'https://api.esoui.com/addons/updatetest' \
+  --header "x-api-token: ${ESOUI_API_KEY}" \
+  --form id=$1 \
+  --form version=$2 \
+  --form archive=Yes \
+  --form compatible=$3 \
+  --form updatefile=@$4
