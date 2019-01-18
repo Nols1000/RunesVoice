4144f7fe05312a5468a966630532f4ac784d9f2a
Nils-Börge Margotti <nilsmargotti@gmail.com>
2019-01-18 14:37:29 +0100

    Generate description and changelog of the markdown-files

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 curl --url 'https://api.esoui.com/addons/update' \
   --header "x-api-token: ${ESOUI_API_KEY}" \
-  --form id=$1 \
-  --form version=$2 \
-  --form archive=Yes \
-  --form compatible=$3 \
-  --form updatefile=@$4
+  --form "id=2173" \
+  --form "version=$1" \
+  --form "archive=Yes" \
+  --form "compatible=$2" \
+  --form "updatefile=@$3" \
+  --form "description=$(.travis/blackfriday -common-extensions -format=vb-code Readme.md)" \
+  --form "changelog=$(.travis/blackfriday -common-extensions -format=vb-code Changelog.md)"
